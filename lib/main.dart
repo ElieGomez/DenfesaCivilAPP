c3a0875e861cc7a8bb6b31ec51b630de346e915f
Rusbel Rodriguez <rusbelrodriguez50@gmail.com>
2024-04-07 21:47:44 -0400

    Prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:defensacivilapp/screens/WagnerEsGei.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,16 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Defenza civil',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Defenza civil', home: ElGei());
   }
 }
---
+++ lib/screens/WagnerEsGei.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors, file_names
+
+import 'package:flutter/material.dart';
+
+class ElGei extends StatelessWidget {
+  const ElGei({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("hola"),
+    );
+  }
+}
