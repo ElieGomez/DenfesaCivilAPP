13cbcee6ac383df92f221ccc158998d984f4dc47
Wesley Jones Novas Mercedes <20220680@itla.edu.do>
2024-04-14 11:00:07 -0400

    Se modifico la UI de la parte de la configuracion

--- lib/screens/Configuracion.dart
+++ lib/screens/Configuracion.dart
@@ -13,52 +13,81 @@ class ConfiguracionView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Configuración'),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        title: Text(
+          'Configuración',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
       ),
-      body: ListView(
-        children: <Widget>[
-          if (tokenProvider.token == '')
-            ListTile(
-              title: Text('Login'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InicioSesion()),
-                );
-              },
-            ),
-          if (tokenProvider.token == '')
-            ListTile(
-              title: Text('Registrarse'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SingUp()),
-                );
-              },
-            ),
-          if (tokenProvider.token != '')
-            ListTile(
-              title: Text('Cambiar contraseña'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChangePassword()),
-                );
-              },
-            ),
-          if (tokenProvider.token != '')
-            ListTile(
-              title: Text('Cerrar Sesion'),
-              onTap: () {
-                Provider.of<AuthToken>(context, listen: false).setToken('');
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InicioSesion()),
-                );
-              },
-            ),
-        ],
+      body: Container(
+        color: Color.fromARGB(255, 164, 161, 161),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(height: 0), // Espacio adicional en la parte superior
+            if (tokenProvider.token == '')
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => InicioSesion()),
+                  );
+                },
+                child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+              ),
+            SizedBox(height: 16.0),
+            if (tokenProvider.token == '')
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SingUp()),
+                  );
+                },
+                child: Text('Registrarse', style: TextStyle(color: Colors.white, fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+              ),
+            // Espacio adicional entre los botones y la parte inferior
+            if (tokenProvider.token != '')
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ChangePassword()),
+                  );
+                },
+                child: Text('Cambiar contraseña', style: TextStyle(color: Colors.white, fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+              ),
+            SizedBox(height: 16.0),
+            if (tokenProvider.token != '')
+              ElevatedButton(
+                onPressed: () {
+                  Provider.of<AuthToken>(context, listen: false).setToken('');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => InicioSesion()),
+                  );
+                },
+                child: Text('Cerrar Sesión', style: TextStyle(color: Colors.white, fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
